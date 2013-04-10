5525287183feae0a8d06c3c0d8b8c3a472781589
Scott Cheloha <scoman99@gmail.com>
2013-04-09 21:24:57 -0400

    fixed wrapper.c|h to not brutally exit the program on failure

--- wrapper.h
+++ wrapper.h
@@ -1,4 +1,4 @@
-/* CMPSC 311, Spring 2013, Project 5
+/* CMPSC 311, Spring 2013, Project 7
  *
  * Author:   Scott Cheloha
  * Email:    ssc5145@psu.edu
@@ -6,31 +6,11 @@
  * originally by: Don Heller
  */
 
-
-/* base library for CMPSC 311 projects
- * version of 25 Feb. 2013
- */
-
-#ifndef CMPSC311_H
-#define CMPSC311_H
+#ifndef WRAPPER_H
+#define WRAPPER_H
 
 #include <stdio.h>
-#include <stdbool.h>
-
-//------------------------------------------------------------------------------
-
-// global variables set from the command line
-
-extern char *prog;	// program name
-extern int verbose;	// -v option, extra output
-			// -v can be repeated for even more output
-
-  // verbosity levels
-  // 0  off, default
-  // 1  normal, behavioral info
-  // 2  extra, functionality info
-  // 3  memory allocation info, from Malloc(), Strdup()
-  // 4  too much, information overload
+#include <sys/types.h>
 
 //------------------------------------------------------------------------------
 
@@ -51,24 +31,19 @@ void cmpsc311_verify(const bool tf,
 
 //------------------------------------------------------------------------------
 
-// check function return values
-//   function	standards
-//   malloc	C and Posix
-//   strdup	      Posix
-//   fopen	C and Posix
-// We follow the standard protoypes of the original functions.
-// Compare these to the error-checking wrappers in CS:APP, csapp.h and csapp.c.
+// Error-checking wrappers for some common functions
+//
 
-#define Malloc(size)         cmpsc311_malloc(size, __func__, __LINE__)
-#define Strdup(s)            cmpsc311_strdup(s, __func__, __LINE__)
-#define Fopen(filename,mode) cmpsc311_fopen(filename, mode, __func__, __LINE__)
+void *Malloc(size_t size,
+	const char *func, const int line);
 
-void *cmpsc311_malloc(size_t size,
+char *Strdup(const char *s,
 	const char *func, const int line);
-char *cmpsc311_strdup(const char *s,
+
+FILE *Fopen(const char * restrict filename, const char * restrict mode,
 	const char *func, const int line);
-FILE *cmpsc311_fopen(const char * restrict filename, const char * restrict mode,
-	const char *func, const int line);
+
+pid_t Fork(void);
 
 //------------------------------------------------------------------------------
 
